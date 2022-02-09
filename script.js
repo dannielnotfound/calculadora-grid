65ac5783b7b31cfec0bdf3b9c25e752b605f5a41
Daniel <87285520+dannielnotfound@users.noreply.github.com>
2022-02-09 10:05:34 -0300

    Revisão do código
    Tornei o CSS e JS como arquivos externos, a fins de maior compreensão caso algum contribuidor deseje participar deste pequeno projeto.

---
+++ script.js
@@ -0,0 +1,31 @@
+function insertValue(num){
+    if(num == 'ac'){ //apagar o display
+        document.getElementById('display').value = ''
+    }else{ //concatenar os numeros 
+        document.getElementById('display').value += num
+    }
+}
+function calcular(){
+    const input = document.getElementById('display').value
+    document.getElementById('display').value = eval(input)
+}
+
+function invetValue(){
+    calcular()
+    const input = document.getElementById('display').value
+    document.getElementById('display').value = input * -1
+    
+}
+
+
+
+
+/* Disparador de evento click */ // Não serve para nada, mas achei interessante 
+var items = document.getElementsByClassName('item')
+for(i = 0; i < items.length; i++){
+    items[i].addEventListener('click', function(){
+        console.log('click')
+    })
+}
+
+
